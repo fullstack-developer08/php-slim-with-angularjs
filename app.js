3c1687f539d90d821df883d48316343122efb7e6
mdarif-k <tech.aarifkhan@gmail.com>
2017-11-19 13:29:06 +0530

    angular4

--- app.js
+++ app.js
@@ -2,15 +2,12 @@
  angular.module('app',['ngRoute'])
  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
         $routeProvider
-        .when('/cart/index.html', {
+        .when('/cart', {
             templateUrl: '/cart/index.template.html'
         })
-        .otherwise({
-            redirectTo: '/cart/index.html'
-        });
         $locationProvider.html5Mode({
             enabled: true,
-            requireBase: false,
+            requireBase: true,
         });
     }])
 })();
